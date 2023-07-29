b025d0f9d0a648b1dd1573c211f3691778d0f000
DATA <dev.katimgouda@gmail.com>
2023-07-29 20:25:38 +0300

    backend

---
+++ resources/views/frontend/layouts/app.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="@if(app()->getLocale() == 'en')'en' @else 'ar' @endif" dir="@if(app()->getLocale() == 'en')'ltr' @else 'rtl' @endif" >
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title> @yield('title') </title>
+    @include('frontend.layouts.assets.style')
+</head>
+<body>
+<div class="container-xxl bg-white p-0">
+    <!-- Spinner Start -->
+    <div id="spinner"
+         class="show bg-white position-fixed translate-middle w-100 vh-100 top-50 start-50 d-flex align-items-center justify-content-center">
+        <div class="spinner-grow text-primary" style="width: 3rem; height: 3rem;" role="status">
+            <span class="sr-only">Loading...</span>
+        </div>
+    </div>
+
+    @include('frontend.layouts.partials._header')
+
+
+    @yield('content')
+
+
+    @include('frontend.layouts.partials.footer')
+    <a href="#" class="btn btn-lg btn-secondary btn-lg-square back-to-top"><i class="bi bi-arrow-up"></i></a>
+</div>
+@include('frontend.layouts.assets.script')
+</body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+use App\Http\Controllers\Frontend\HomeController;
+use Illuminate\Support\Facades\Route;
+use Mcamara\LaravelLocalization\Facades\LaravelLocalization;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+Route::group(
+    [
+        'prefix' => LaravelLocalization::setLocale(),
+        'middleware' => [ 'localeSessionRedirect', 'localizationRedirect', 'localeViewPath' ]
+    ], function(){
+        Route::prefix('/')->as('index')->controller(HomeController::class)->group(function (){
+            Route::get('/','index')->name('index');
+        });
+    });
